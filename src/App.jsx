9a6b1beb3183e17a8cd1719b32f3ef6df200b312
defendimattia <defendi.mattia92@gmail.com>
2025-04-14 15:01:45 +0200

    add: useState logic to inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import { useState } from 'react'
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false
+  })
+
+  function handleFormData(e) {
+    const value =
+      e.target.type === "checkbox" ?
+        e.target.checked : e.target.value;
+    setFormData((formData) => ({
+      ...formData,
+      [e.target.name]: value,
+    }));
+  }
+
   return (
 
     <>
       <h1>Post Form</h1>
+
       <form>
-        <input type="text" /> author<br />
-        <input type="text" /> title<br />
-        <input type="text" /> body<br />
-        public <input type="checkbox" />
+
+        <label htmlFor="author">author</label>
+        <input id="author" type="text" name="author" value={formData.author} onChange={handleFormData} /> 
+        <br />
+
+        <label htmlFor="title">title</label>
+        <input id="title" type="text" name="title" value={formData.title} onChange={handleFormData} />
+        <br />
+
+        <label htmlFor="body">body</label>
+        <input id="body" type="text" name="body" value={formData.body} onChange={handleFormData} />
+        <br />
+
+        <label htmlFor="author">public</label>
+        <input id="public" type="checkbox" name="public" checked={formData.public} onChange={handleFormData} />
+
       </form>
     </>
 
