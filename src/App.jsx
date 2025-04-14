d9ccc78383031b5fe0dc16fab4d09f39413c7ef6
defendimattia <defendi.mattia92@gmail.com>
2025-04-14 14:42:29 +0200

    add: html structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import { useState } from 'react'
 
 function App() {
   return (
+
     <>
+      <h1>Post Form</h1>
+      <form>
+        <input type="text" /> author<br />
+        <input type="text" /> title<br />
+        <input type="text" /> body<br />
+        public <input type="checkbox" />
+      </form>
     </>
+
   )
 }
 
